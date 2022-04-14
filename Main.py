23a19b1a6c809e1c954732511c1876e95a52268c
11081999 <robertoriverateran@gmail.com>
2022-04-14 18:09:54 -0500

    f
    f

--- Main.py
+++ Main.py
@@ -1,42 +1,100 @@
-#Roberto Rivera terán
-#Archana Verma Rodriguez
-#Audry
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
 
-##Import wx
-##GUI: https://www.blog.pythonlibrary.org/2021/09/29/create-gui/
+class MyApp(tk.Tk):
 
-import wx
-class MyPanel(wx.Panel):
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+        main_frame = tk.Frame(self, bg="#84CEEB", height=600, width=1024)
+        main_frame.pack_propagate(0)
+        main_frame.pack(fill="both", expand="true")
+        main_frame.grid_rowconfigure(0, weight=1)
+        main_frame.grid_columnconfigure(0, weight=1)
+        self.resizable(0, 0) #prevents the app from being resized
+        self.geometry("1024x600") #fixes the applications size
+        self.frames = {}
+        pages = (PageOne, PageTwo)
+        for F in pages:
+            frame = F(main_frame, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        #self.show_frame(Some_Widgets)
+        menubar = MenuBar(self)
+        tk.Tk.config(self, menu=menubar)
+
+    def show_frame(self, name):
+        frame = self.frames[name]
+        frame.tkraise()
+
+    #def OpenNewWindow(self):
+        #OpenNewWindow()
+
+    def Quit_application(self):
+        self.destroy()
+
+class GUI(tk.Frame):
     def __init__(self, parent):
-        super().__init__(parent)
-        button = wx.Button(self, label='Press Me')
-        button.Bind(wx.EVT_BUTTON, self.on_button1)
+        tk.Frame.__init__(self, parent)
+        self.main_frame = tk.Frame(self, bg="#BEB2A7", height=600, width=1024)
+        # self.main_frame.pack_propagate(0)
+        self.main_frame.pack(fill="both", expand="true")
+        self.main_frame.grid_rowconfigure(0, weight=1)
+        self.main_frame.grid_columnconfigure(0, weight=1)
 
-        button2 = wx.Button(self, label='Second button')
-        button2.Bind(wx.EVT_BUTTON, self.on_button2)
+class MenuBar(tk.Menu):
+    def __init__(self, parent):
+        tk.Menu.__init__(self, parent)
 
-        main_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        main_sizer.Add(button, proportion=1,
-                       flag=wx.ALL | wx.CENTER | wx.EXPAND,
-                       border=5)
-        main_sizer.Add(button2, 0, wx.ALL, 5)
-        self.SetSizer(main_sizer)
+        menu_file = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="Menu1", menu=menu_file)
 
-    def on_button1(self, event):
-        print('You clicked the first button')
+        menu_file.add_command(label="Page One", command=lambda: parent.show_frame(PageOne))
+        menu_file.add_separator()
+        menu_file.add_command(label="Exit Application", command=lambda: parent.Quit_application())
+        """
+        menu_orders = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="Menu2", menu=menu_orders)
 
-    def on_button2(self, event):
-        print('You clicked the second button')
+        menu_pricing = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="Menu3", menu=menu_pricing)
+        menu_pricing.add_command(label="Page One", command=lambda: parent.show_frame(PageOne))
 
-class MyFrame(wx.Frame):
-    def __init__(self):
-        super().__init__(None, title='Hello World')
-        panel = MyPanel(self)
-        self.Show()
+        menu_operations = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="Menu4", menu=menu_operations)
+        menu_operations.add_command(label="Page Two", command=lambda: parent.show_frame(PageTwo))
+        menu_positions = tk.Menu(menu_operations, tearoff=0)
+        menu_operations.add_cascade(label="Menu5", menu=menu_positions)
+        menu_positions.add_command(label="Page Three", command=lambda: parent.show_frame(PageThree))
+        menu_positions.add_command(label="Page Four", command=lambda: parent.show_frame(PageFour))
 
-if __name__ == '__main__':
-    app = wx.App(redirect=False)
-    frame = MyFrame()
-    app.MainLoop()
+        menu_help = tk.Menu(self, tearoff=0)
+        self.add_cascade(label="Menu6", menu=menu_help)
+        menu_help.add_command(label="Open New Window", command=lambda: parent.OpenNewWindow())
+        """
 
 
+class PageOne(GUI):
+    def __init__(self, parent, controller):
+        GUI.__init__(self, parent)
+
+        label1 = tk.Label(self.main_frame, font=("Verdana", 20), text="Page One")
+        label1.pack(side="top")
+
+class PageTwo(GUI):
+    def __init__(self, parent, controller):
+        GUI.__init__(self, parent)
+
+        label1 = tk.Label(self.main_frame, font=("Verdana", 20), text="Page Two")
+        label1.pack(side="top")
+
+
+top = MyApp()
+top.title("Tkinter App Template - Login Page")
+root = MyApp()
+root.withdraw()
+root.title("Tkinter App Template")
+
+root.mainloop()
